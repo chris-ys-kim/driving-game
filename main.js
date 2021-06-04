afa99acacdefdea0f69ea0899e485aaf7907cd98
Chris Kim <midaskim6969@yahoo.com>
2021-06-04 16:38:00 -0700

    completed user-can-start-their-car.

--- main.js
+++ main.js
@@ -1,9 +1,27 @@
-var $car = document.querySelector('.right');
+var $carR = document.querySelector('.right');
+var intervalId = setInterval(move, 16);
+var left = 0;
+var space = 0;
+
+function move() {
+  left += 16;
+  $carR.style.left = left + 'px';
+}
 
 window.addEventListener('keydown', function (event) {
-  if (event.key === 'ArrowRight') { $car.className = 'right'; }
-  if (event.key === 'ArrowLeft') { $car.className = 'left'; }
-  if (event.key === 'ArrowUp') { $car.className = 'top'; }
-  if (event.key === 'ArrowDown') { $car.className = 'bottom'; }
-
+  if (event.key === 'ArrowRight') { $carR.className = 'right'; }
+  if (event.key === 'ArrowLeft') { $carR.className = 'left'; }
+  if (event.key === 'ArrowUp') { $carR.className = 'top'; }
+  if (event.key === 'ArrowDown') { $carR.className = 'bottom'; }
+  if (event.key === ' ') {
+    space++;
+    if (space % 2 === 1) {
+      intervalId = setInterval(move, 16);
+    }
+    if (space % 2 === 0) {
+      clearInterval(intervalId);
+    }
+  }
 });
+
+clearInterval(intervalId);
